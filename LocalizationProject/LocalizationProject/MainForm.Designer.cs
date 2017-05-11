f51ded0d2bccaee08a98d8cfdd9151d6559e7e1d
Danya Goryachev <danyagor98@gmail.com>
2017-05-11 15:46:19 +0300

    Проект локализации

---
+++ LocalizationProject/LocalizationProject/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace LocalizationProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStrings = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbLanguages = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbStrings
+            // 
+            this.lbStrings.FormattingEnabled = true;
+            this.lbStrings.Location = new System.Drawing.Point(12, 12);
+            this.lbStrings.Name = "lbStrings";
+            this.lbStrings.Size = new System.Drawing.Size(172, 290);
+            this.lbStrings.TabIndex = 0;
+            this.lbStrings.SelectedIndexChanged += new System.EventHandler(this.lbStrings_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(193, 68);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(219, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // tbValue
+            // 
+            this.tbValue.Location = new System.Drawing.Point(193, 107);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(219, 20);
+            this.tbValue.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Значение";
+            // 
+            // cbLanguages
+            // 
+            this.cbLanguages.FormattingEnabled = true;
+            this.cbLanguages.Location = new System.Drawing.Point(193, 28);
+            this.cbLanguages.Name = "cbLanguages";
+            this.cbLanguages.Size = new System.Drawing.Size(219, 21);
+            this.cbLanguages.TabIndex = 6;
+            this.cbLanguages.SelectedIndexChanged += new System.EventHandler(this.cbLanguages_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Язык";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 315);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbLanguages);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbValue);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbStrings);
+            this.Name = "Form1";
+            this.Text = "Программа локализации Коллекций";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbStrings;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbLanguages;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LocalizationProject/LocalizationProject/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace LocalizationProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillLanguagesComboBox();
+        }
+        
+        private void FillLanguagesComboBox()
+        {
+            DirectoryInfo dir = new DirectoryInfo("Languages");
+            FileInfo[] files = dir.GetFiles("*.xml");
+
+            cbLanguages.Items.Clear();
+
+            for (int i = 0; i < files.Length; i++)
+                cbLanguages.Items.Add(files[i].Name);    
+
+            if (cbLanguages.Items.Count != 0)
+                cbLanguages.SelectedIndex = 0;
+        }
+
+        private void FillStringsListBox()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Languages\\" + cbLanguages.Text);
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            lbStrings.Items.Clear();
+
+            foreach (XmlNode node in xRoot.ChildNodes)
+                lbStrings.Items.Add(node.Attributes["name"].Value);
+        }
+
+        private void cbLanguages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillStringsListBox();
+        }
+
+        private void lbStrings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Languages\\" + cbLanguages.Text);
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            tbName.Text = xRoot.ChildNodes[lbStrings.SelectedIndex].Attributes["name"].Value;
+            tbValue.Text = xRoot.ChildNodes[lbStrings.SelectedIndex].Attributes["value"].Value;
+        }
+    }
+}
