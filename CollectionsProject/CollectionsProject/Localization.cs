4c8247c8ae30bd061b1bc38378eb498c797981e7
Danya Goryachev <danyagor98@gmail.com>
2017-05-18 11:52:05 +0300

    Начало создания новой структуры коллекций

--- CollectionsProject/CollectionsProject/Localization.cs
+++ CollectionsProject/CollectionsProject/Localization.cs
@@ -12,7 +12,7 @@ namespace CollectionsProject
         static string GetValue(string name)
         {
             XmlDocument xDoc = new XmlDocument();
-            xDoc.Load("russain.xml");
+            xDoc.Load("russian.xml");
             XmlElement xRoot = xDoc.DocumentElement;
             foreach (XmlNode item in xRoot.ChildNodes)
             {
